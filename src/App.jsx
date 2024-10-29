8104ed576046eff7b36359f752f87324d1630b02
Renan Alves <renanlvsdv@gmail.com>
2024-10-29 03:58:18 -0300

    Primeiro commit, projeto iniciado, construção de header em andamento

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Components/Header';
+import './app.css';
+
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	);
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../assets/logo-barber.svg';
+import './styles/Header.css';
+
+const Header = () => {
+	return (
+		<header>
+			<div className="containerLogo">
+				<img src={logo} alt="logo da barber shop" width={40} />
+				<span>Barber Shop</span>
+			</div>
+
+			<nav className="containerNav">
+				<li>Home</li>
+				<li>About</li>
+				<li>Gallery</li>
+				<li>Contact</li>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
