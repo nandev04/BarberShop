dbcf62e618504daf1fbf75776f328b89db2fc116
Renan Alves <renanlvsdv@gmail.com>
2024-12-10 04:01:32 -0300

    atualizado estrutura de navegação

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,15 +11,17 @@ const Header = () => {
 			</div>
 
 			<nav className="containerNav">
-				<li>
-					<a href="#about">About</a>
-				</li>
-				<li>
-					<a href="#gallery">Gallery</a>
-				</li>
-				<li>
-					<a href="#contact">Contact</a>
-				</li>
+				<ul>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#gallery">Gallery</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	);
